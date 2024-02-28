7747d4373c4d5d0009315962b73212d84fc0d134
Christian E <christianelena92@gmail.com>
2024-02-28 00:08:23 -0600

    Create a filtermethod when user types in a city in the input text field, trim the string passed in, Bind click event to filter out the object to find the names inputted by user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,13 +12,13 @@ import { HousingLocation } from '../housingLocation';
   template: `
   <section class="mx-auto mt-6">
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary hover:bg-blue-700" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary hover:bg-blue-700" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
 <app-housing-location
-  *ngFor="let housingLocation of housingLocationList"
+  *ngFor="let housingLocation of filteredLocationList"
   [housingLocation]="housingLocation">
 </app-housing-location>
   </section>
@@ -30,7 +30,19 @@ export class HomeComponent {
 
   housingLocationList: HousingLocation[] = []; 
   housingService: HousingService = inject(HousingService); 
+  filteredLocationList: HousingLocation[] = []; 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations(); 
+    this.filteredLocationList = this.housingLocationList; 
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
